f43b3f9e8519d531559b26db6fc3836ddc83a1b4
jaredap1995 <118015190+jaredap1995@users.noreply.github.com>
2023-03-12 13:55:40 -0700

    Update workout_app.py

--- workout_app.py
+++ workout_app.py
@@ -9,21 +9,69 @@ import re
 import streamlit as st
 from clean_workouts import clean_workouts
 from grab_all_workouts import grab_all_workouts
+from intensity_classification import intensity_classification
 
-name=st.text_input("Enter Your First Name: ")
-show_workout = st.button("Show Workout")
+def show_new_workout():
+    st.write("Feature coming soon! :)")
 
-if show_workout:
-    if name: 
-        files = grab_all_workouts(name)
-        files = clean_workouts(files)
-        workout = random.choice(files)
-        st.text("Don't like the workout? Just hit the button again!")
-        st.dataframe(workout)
+def load_workouts(name):
+    files = grab_all_workouts(name)
+    files = clean_workouts(files)
+    files = intensity_classification(files)
+
+    return files
+
+
+def show_existing_workout(files, selected_intensity):
+    intensities = list(set(df.Intensity for df in files))
+    filtered_workouts = [df for df in files if df.Intensity == selected_intensity]
+    workout = random.choice(filtered_workouts)
+    return workout
+        
+    
+
+def app():
+    st.set_page_config(page_title="Cabral Fitness Exercise Prescriptions")
+
+    # Define the home page
+    st.header("Welcome!")
+
+    # Define the "New Workout" button
+    new_workout=st.button('New Workout')
+    if new_workout:
+        show_new_workout()
+
+    # Define the "Existing Workout" button
+    existing_workout_button = st.button('Existing Workout')
+
+    if 'new_workout' not in st.session_state:
+        st.session_state['new_workout']= False
+
+    if 'existing_workout' not in st.session_state:
+        st.session_state['existing_workout']=False
+
+    st.write(st.session_state)
+
+    
+    if existing_workout_button or st.session_state.existing_workout:
+        st.session_state.existing_workout=True
+        with st.form(key='existing_workout_form'):
+            name = st.text_input('Enter Your First Name:')
+            intensities=['Moderate-Heavy', 'Moderate-Light', 'Light', 'Moderate', 'Heavy']
+            intensity = st.selectbox('Select intensity level:', intensities)            
+            submit_button = st.form_submit_button(label='Produce Workout')
+            if submit_button:
+                files=load_workouts(name)
+                workout=show_existing_workout(files, intensity)        
+                st.text("Don't like the workout? Just hit the button again!")
+                st.dataframe(workout)
+
         
 
-    else:
-        st.warning("Please enter your name")
+
+app()
+
+
 
 
 
