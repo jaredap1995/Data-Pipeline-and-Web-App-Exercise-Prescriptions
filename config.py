c36125f9e56e329e86ad9fa3ae716e935e261520
jaredap1995 <jaredaperez1995@gmail.com>
2023-03-14 21:15:55 -0700

    ignored

---
+++ config.py
@@ -0,0 +1,13 @@
+SERVICE_KEY= """{
+  "type": "service_account",
+  "project_id": "my-first-project-streamlit",
+  "private_key_id": "f6c94ed784d074ffe6ad8141bf3c4af86782145f",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCvdjxXAuZ8i94p\nEdYnovgKiH1jt5EpufV6JqEADDJz+jqRJ6lU7mv1cjJO3K0LHSz0PwJp/zxGVp80\nfQCIMhG2KV02XnSWonWDG2HHWNr9tBQ3q1vMdDYjQMG2TUd/i9GSZ+Jg7jCffs8K\nBHNy9bQwD8ysV5mb8iV4V4Rvl8kfYV6l9wC13ISNaCN1f3EDohzlP8hOGH2OkqZG\nLRlguiMWNtCSOpYfRcFa1TQcM56VxshQBILntdWaokdVoimSwdzNTbi8A0P3crd8\n9S35vu2d50pkenOKOhSrSvoRzBiAeZ83CGJ1FIyrn9JcG7OeZWb28pBAclRCQQnm\nozxRy9gHAgMBAAECggEATyQNY1GzlEmiEvQwnAAt0NJvfCK4HxH5Xvc9d0AJIbaj\n5AT3o49GspanilqO0ox+Ukx5UkYFcdRwSsR3EPdEdd2CZZuFCsyMJG6dU8UIQZaP\n5+wqo7VnFc+HkCqbLTRrepXQzdehps8To3kOebPJakQ2FSYN6wD1Vapj2dGkrboE\nN6UqoZOPzypR9VtI9jlq3xYHWrN/Ak/YAQrCdCdR3RMKGtg6l7hJAagMXUgGdFit\nvEg+9f0mG99pi+N8fI6u9vRd3RIGMccWVgIdmJIRYyAbGGY0Ynt0s2vM9ZytiYQh\nufq1fyVhaHjLYDSS9wrGT6bzd0HLPDLCtkQUnoqAQQKBgQD1HfRWlQU1/GbP2k6F\neYcnC4jsASsUHB1P7MTnjt1SAU/UB1EWaW1ka9TxSJ8z7rfH+qVGpJE27ryt1EgU\nA9yZlrrO5RC0anmyzmQjPagqRn3L1o3xNcUgXWp2VJJ57i2kJJpZwSPW8nsEYpG7\n0Yoa+Et38+pDNaIoZZPsXnq1cwKBgQC3QJFeEQ9p+pLhStjOkPGWykGlxMMQtv0u\nS827ofkIMVXG3eSIXwJF2HmXptq00Q0ukkoBu8xRaYufpJLBGiBmSm3f63TIvs+E\nU3Ssjl173atUFGP7A75msyoFN2z//64OoEhZQ0LxmifAHArOefHWMWQ1wnl+4URW\ncdP7MeMOHQKBgQCZgKLNvhE/V8vDC0H1UMAU8aJTuf+SXnD05qABK3TqsCuf6t0b\nHfcF1onKeOJKkYrCT/NFJDZUonnCmphxAGIcRUrD2qv1cGC6W4pWj3xKzU5mYv0N\nO9ExjY7of9ojRceLbejDoLKjH8NuYZRGfNjyPuB0RGpGoMXmaoaSljBehQKBgQCd\nLv8BprSH/Ox73o11MZJn+viwTcPb1Hn6ef7wEJDhXB/tQmtCJex3KKWzVOWH70DV\nT5m3PvSUaGng93MBu31LAFCanwY/IpwxLryHPl4lXxAUQKE5sA5jXQ6QBuix9QfB\nd+5W0oDbZ5TtDaCfVq3/QcJo8nTwRh0t4NkpP7M4aQKBgQCdOhr2+MhjaulZiq6Q\nXI7GSk/4WvqQTfJ11hYcU7zmR1ksk7OQYHUS/xs4CNMfY/46D6546v5P9LjBe2UU\nKLHbqsSeWy7UPxzUxSavass/8fJ8ZQdjBn1hOWl7H6yCtvpfiXKS0subhr98JLsi\n/NESmMpSaMKHAvR3vGyRxZ4kQA==\n-----END PRIVATE KEY-----\n",
+  "client_email": "my-service-account@my-first-project-streamlit.iam.gserviceaccount.com",
+  "client_id": "113073596655395312787",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/my-service-account%40my-first-project-streamlit.iam.gserviceaccount.com"
+}
+"""
