efedb914adf3a3c7cb8ab322bedb7178e57c0e58
jaredap1995 <jaredaperez1995@gmail.com>
2023-03-22 08:20:25 -0700

    k

--- workout_app.py
+++ workout_app.py
@@ -38,9 +38,11 @@ def app():
         # Start the Cloud SQL proxy when the application starts
         #proxy_process = subprocess.Popen(
         #['./cloud-sql-proxy', '--address', st.secrets.proxy_credentials.address, '--port', st.secrets.proxy_credentials.port, st.secrets.proxy_credentials.name])
-    conn = psycopg2.connect(**st.secrets.psycopg2_credentials)
-    if conn:
-        time.sleep(5) 
+    @st.cache_resource
+    def init_connection():
+        return psycopg2.connect(**st.secrets.psycopg2_credentials)
+
+    conn = init_connection()
 
 
     cursor = conn.cursor()
