c3c6e4658356256c60764faa7df4ff4e76a54b8f
jaredap1995 <jaredaperez1995@gmail.com>
2023-04-16 16:15:35 -0700

    new test branch and maintain current workout status function testing

--- testing_coach_and_prescriptions.py
+++ testing_coach_and_prescriptions.py
@@ -2,6 +2,7 @@ from coach_center import deload, increasing_load, create_a_block, coach
 from record_prescriptions import record_block
 import streamlit as st
 import psycopg2
+import time
 
 
 
@@ -15,7 +16,10 @@ def prescribe_block(conn, name):
     # submit_button=st.button('Record Block in Database')
     if st.session_state.block:
         record_block(conn, name, st.session_state['block'], length_of_block)
-        st.success('Block Uploaded to Database Successfully')
+        st.success('Block Uploaded to Database Successfully. Rerunning to update state.')
+        time.sleep(4)
+        st.session_state['block']=[]
+        st.experimental_rerun()
         
 
 
