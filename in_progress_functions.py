40afb4a6d777342c51eaecd1faa642b0b2ce7745
jaredap1995 <jaredaperez1995@gmail.com>
2023-04-16 17:17:28 -0700

    More debugging attempts

--- in_progress_functions.py
+++ in_progress_functions.py
@@ -42,6 +42,7 @@ def test(in_progress, name, notes, workout_number):
     # session_id=cursor.fetchone()[0]
     session_id=int(workout_number)
 
+    st.write(session_id)
     cursor.execute("""
         CREATE UNIQUE INDEX IF NOT EXISTS idx_exercise_sets_reps_weight 
         ON in_progress (exercise_id, sets, reps, weight)
