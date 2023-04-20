46ec3e983a15280b3146b60272be3048568d5934
jaredap1995 <jaredaperez1995@gmail.com>
2023-04-20 13:12:59 -0700

    if not workaround"

--- track_block_progress.py
+++ track_block_progress.py
@@ -292,12 +292,11 @@ def show_progress_in_block(conn, name):
                         try:
                             #df['Done']=[False for _ in range(len(df.index))]
                             notes=None
-                            if f'edited_df{number}' not in st.session_state:
-                                st.session_state[f'edited_df{number}'] = df
-                            st.session_state[f'edited_df{number}'] = st.experimental_data_editor(st.session_state[f'edited_df{number}'], num_rows='dynamic', on_change=update_in_progress_workout, args=(conn, st.session_state[f'edited_df{number}'], name, workout_number_column[0], notes))
                             
+                            edited_df=st.experimental_data_editor(df, key=f"editor{number}", num_rows='dynamic') #on_change=update_in_progress_workout, args=(conn, edited_df, name, workout_number_column[0], notes))
                             notes=st.text_input('Workout Notes', key=f"notes_{number}")
-                            #update_in_progress_workout(conn, edited_df, name, workout_number_column[0], notes)
+                            if not (edited_df.equals(df)):
+                                update_in_progress_workout(conn, edited_df, name, workout_number_column[0], notes)
                             store_performed_workout=st.button(f'Submit Workout Number {number+1}')
                         except st.errors.DuplicateWidgetID:
                             pass
