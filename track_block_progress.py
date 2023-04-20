c7037bbabac9746ef0fec8787988d78244141306
jaredap1995 <jaredaperez1995@gmail.com>
2023-04-20 13:05:19 -0700

    trying again

--- track_block_progress.py
+++ track_block_progress.py
@@ -294,7 +294,7 @@ def show_progress_in_block(conn, name):
                             notes=None
                             if f'edited_df{number}' not in st.session_state:
                                 st.session_state[f'edited_df{number}'] = df
-                            edited_df = st.experimental_data_editor(st.session_state[f'edited_df{number}'], num_rows='dynamic', on_change=update_in_progress_workout, args=(conn, st.session_state[f'edited_df{number}'], name, workout_number_column[0], notes))
+                            st.session_state[f'edited_df{number}'] = st.experimental_data_editor(st.session_state[f'edited_df{number}'], num_rows='dynamic', on_change=update_in_progress_workout, args=(conn, st.session_state[f'edited_df{number}'], name, workout_number_column[0], notes))
                             
                             notes=st.text_input('Workout Notes', key=f"notes_{number}")
                             #update_in_progress_workout(conn, edited_df, name, workout_number_column[0], notes)
