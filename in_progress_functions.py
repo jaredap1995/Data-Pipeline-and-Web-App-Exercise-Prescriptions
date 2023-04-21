1d254f32dc8b7e7b91f845e0bb4c0b63f4bfccac
jaredap1995 <jaredaperez1995@gmail.com>
2023-04-20 20:29:34 -0700

    debugging

--- in_progress_functions.py
+++ in_progress_functions.py
@@ -136,7 +136,7 @@ def check_if_in_progress_exists(conn, name):
                     st.write(st.session_state['continued_workout'])
                     # diff_rows = continued_workout[continued_workout != df_2].dropna(how='all').index
                     # diff_rows=continued_workout.loc[diff_rows]
-                    update_in_progress_workout(conn, st.session_state['continued_workout'], name, workout_number)
+                    update_in_progress_workout(conn, continued_workout, name, workout_number)
             except ValueError as e:
                 if str(e) == "Cannot mask with non-boolean array containing NA / NaN values":
                     st.error("Make sure to hit the checkbox after entering a new exercise")
