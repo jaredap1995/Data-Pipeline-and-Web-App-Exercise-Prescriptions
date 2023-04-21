993cc0462cf2652cd349222d48967ec5e4a5084d
jaredap1995 <jaredaperez1995@gmail.com>
2023-04-21 11:03:10 -0700

    removed the writing of session state df_value

--- in_progress_functions.py
+++ in_progress_functions.py
@@ -58,7 +58,7 @@ def update_in_progress_workout(conn, in_progress, name, workout_number, notes=No
     # st.write(in_progress)
     #st.write(workout_number)
     st.dataframe(in_progress)
-    st.write(st.session_state['df_value'].style.set_properties(**{'background-color': 'lightgreen'}))
+    #st.write(st.session_state['df_value'].style.set_properties(**{'background-color': 'lightgreen'}))
 
     while True:
         if in_progress.shape[0] > 0:
