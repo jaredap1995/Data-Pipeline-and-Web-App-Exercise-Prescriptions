69b80af21522897299090f3020fbcf7e22f56218
jaredap1995 <jaredaperez1995@gmail.com>
2023-04-24 08:07:27 -0700

    debugging

--- visualization_functions.py
+++ visualization_functions.py
@@ -215,7 +215,7 @@ def pull_visuals (conn, name):
     weight_actuals_df = pd.concat(weight_a, axis=1, ignore_index=True)
     st.dataframe(weight_actuals_df)
     
-    weight_actuals_df.columns=pd.Index([f'{index_1[i]}_{[i]}' for i in range(len(index_1))])
+    weight_actuals_df.columns=pd.Index([f'{index_1[i]}_{i}' for i in range(len(index_1))])
     st.dataframe(weight_actuals_df)
     st.stop()
     
