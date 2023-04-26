f823efb0eb6f9071a863ab7c6c7af51ba0de69a6
jaredap1995 <jaredaperez1995@gmail.com>
2023-04-25 19:51:40 -0700

    syntax error

--- visualization_functions.py
+++ visualization_functions.py
@@ -341,7 +341,7 @@ def pull_visuals (conn, name):
     cursor.execute("SELECT id from client where name =%s", (name,))
     client_id=cursor.fetchone()[0]
 
-    cursor.execute("""SELECT DISTINCT we.exercise_id, e.exercise
+    cursor.execute("""SELECT DISTINCT e.exercise
         FROM workout_exercises AS we
         JOIN exercises AS e ON we.exercise_id = e.id
         WHERE we.client_id=%s""", (client_id,))
