52e23f17debade524f75a1d83539d4533472a7e0
jaredap1995 <jaredaperez1995@gmail.com>
2023-05-14 10:23:14 -0700

    .

--- Client_Center.py
+++ Client_Center.py
@@ -74,6 +74,8 @@ def name_function():
 
     # Set up the sidebar for user input
     with st.sidebar:
+        if 'name' in st.session_state and st.session_state['name'] is not None:
+            return st.session_state['name']
         st.success("Please Select your Name from the Dropdown Menu. Or if you are a new client, enter your name!")
         name = st.multiselect('Please select your name', clients, key='client_name')
         if name:
