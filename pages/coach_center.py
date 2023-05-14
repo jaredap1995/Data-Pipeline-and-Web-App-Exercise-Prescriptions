e6d2cbb2ebfdceb3f0d7625185ade382600b79a5
jaredap1995 <jaredaperez1995@gmail.com>
2023-05-14 15:27:17 -0700

    .

--- pages/coach_center.py
+++ pages/coach_center.py
@@ -212,6 +212,7 @@ def prescribe_block(conn, name):
         st.experimental_rerun()
 
 def coach_page():
+    st.set_page_config(layout="wide")  
     home_text=st.markdown(
     """
     # Welcome to the Coaching Webpage!
