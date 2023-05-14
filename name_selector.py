4216818d123d892bf7333c1f58f9b9484f96acbd
jaredap1995 <jaredaperez1995@gmail.com>
2023-05-14 12:50:33 -0700

    .

--- name_selector.py
+++ name_selector.py
@@ -5,51 +5,53 @@ import psycopg2
 def name_function():
 
     name=st.session_state.get('name', None)
-    
-    conn = psycopg2.connect(**st.secrets.psycopg2_credentials)
-    
-    cursor = conn.cursor()
-    cursor.execute("SELECT name FROM client;")
-    clients = [row[0] for row in cursor.fetchall()]
+    if not name:
+        conn = psycopg2.connect(**st.secrets.psycopg2_credentials)
+        
+        cursor = conn.cursor()
+        cursor.execute("SELECT name FROM client;")
+        clients = [row[0] for row in cursor.fetchall()]
 
-    if 'new_name' not in st.session_state:
-        st.session_state['new_name']=False
-    if 'new_submit' not in st.session_state:
-        st.session_state['new_submit']=False
+        if 'new_name' not in st.session_state:
+            st.session_state['new_name']=False
+        if 'new_submit' not in st.session_state:
+            st.session_state['new_submit']=False
 
-    # Set up the sidebar for user input
-    with st.sidebar:
-        st.success("Please Select your Name from the Dropdown Menu. Or if you are a new client, enter your name!")
-        name = st.multiselect('Please select your name', clients, key='client_name')
-        if name:
-            name=name[0]
-            st.session_state['name'] = name
-        new_client = st.button('New client?')
-        if new_client or st.session_state.new_name:
-            st.session_state['new_name']=True
-            new_name = st.text_input('Please enter your first name:', value='')
-            if st.button('Submit') or st.session_state.new_submit:
-                st.session_state['new_submit']=True
-                if new_name != '':
-                    st.session_state['new_name'] = new_name
-                    st.experimental_set_query_params(name=new_name)
-                    cursor.execute("INSERT INTO client(name) VALUES (%s)", (new_name,))
-                    conn.commit()
-                    name=new_name
-                    st.session_state['name'] = name
+        # Set up the sidebar for user input
+        with st.sidebar:
+            st.success("Please Select your Name from the Dropdown Menu. Or if you are a new client, enter your name!")
+            name = st.multiselect('Please select your name', clients, key='client_name')
+            if name:
+                name=name[0]
+                st.session_state['name'] = name
+            new_client = st.button('New client?')
+            if new_client or st.session_state.new_name:
+                st.session_state['new_name']=True
+                new_name = st.text_input('Please enter your first name:', value='')
+                if st.button('Submit') or st.session_state.new_submit:
+                    st.session_state['new_submit']=True
+                    if new_name != '':
+                        st.session_state['new_name'] = new_name
+                        st.experimental_set_query_params(name=new_name)
+                        cursor.execute("INSERT INTO client(name) VALUES (%s)", (new_name,))
+                        conn.commit()
+                        name=new_name
+                        st.session_state['name'] = name
 
-                else: 
-                    st.warning('Please Enter Your Name')
-        else:
-            if st.button('Submit'):
-                st.experimental_set_query_params(name=name)
-                st.experimental_rerun()
+                    else: 
+                        st.warning('Please Enter Your Name')
+            else:
+                if st.button('Submit'):
+                    st.experimental_set_query_params(name=name)
+                    st.experimental_rerun()
 
-        conn.close()
-    
-    # if 'name' not in st.session_state or st.session_state['name'] is None:
-    #     st.session_state['name'] = name
-    # else:
-    #     name = st.session_state['name']
+            conn.close()
+        
+        # if 'name' not in st.session_state or st.session_state['name'] is None:
+        #     st.session_state['name'] = name
+        # else:
+        #     name = st.session_state['name']
 
-    return name
+        return name
+    else:
+        return name
