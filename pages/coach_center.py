81d8fc8990920b1f8f4e58045aaffc5b10d05af2
jaredap1995 <jaredaperez1995@gmail.com>
2023-05-14 10:10:30 -0700

    .

--- pages/coach_center.py
+++ pages/coach_center.py
@@ -216,12 +216,13 @@ if 'name' not in st.session_state:
     st.session_state['name'] = None
     st.session_state['name']=name_function()
 
-    
+
 name=st.session_state['name']
 # st.write(st.session_state['name'])
 
 if not name:
     st.header('No Name Selected. Please select a name from the sidebar.')
+    st.session_state['name']=name_function()
 else:
     st.header(f'You are in Coach Mode and prescribing for {name}. Please Select an Option Below')
     show(st.session_state['name'], st.session_state['conn'])
