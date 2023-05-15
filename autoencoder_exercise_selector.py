799ada07c8556db403c6f8c1f65efb9657749f4b
jaredap1995 <jaredaperez1995@gmail.com>
2023-05-15 11:18:09 -0700

    .

--- autoencoder_exercise_selector.py
+++ autoencoder_exercise_selector.py
@@ -248,6 +248,7 @@ def exercise_selector(conn):
             try:
                 workout_dfs=[]
                 for ex in original_exercise:
+                    st.write(original_exercise)
                     exercise_options=df[df['Exercise']==ex]
                     st.write(exercise_options)
                     VL_range=get_intensity_range(exercise_options, intensity)
