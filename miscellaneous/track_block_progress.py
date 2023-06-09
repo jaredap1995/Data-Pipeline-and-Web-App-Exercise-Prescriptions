6e66375588fcda9cf741cd07c643909e424445e3
jaredap1995 <jaredaperez1995@gmail.com>
2023-06-08 18:21:16 -0700

    .

--- miscellaneous/track_block_progress.py
+++ miscellaneous/track_block_progress.py
@@ -49,8 +49,11 @@ def ordering_function_for_performed_workouts(num_workouts, num_weeks, dfs, actua
 
         order=pd.Series(order)
         # apply the ordering to the "Exercise" column of the dataframe
-        df['Exercise'] = pd.Categorical(df['Exercise'], categories=order, ordered=True)
-        df.sort_values('Exercise', inplace=True)
+        try:
+            df['Exercise'] = pd.Categorical(df['Exercise'], categories=order, ordered=True)
+            df.sort_values('Exercise', inplace=True)
+        except:
+            pass
         
         
     return actuals
